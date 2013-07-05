3840a6305366b71c0a6efa45fb0c8159b52d9b77
HUNG YU-TING <r96941046@gmail.com>
2013-07-05 22:32:13 +0800

    indexTemp test server

--- app.js
+++ app.js
@@ -2,14 +2,27 @@
 /* experiments testing node.js and npm */
 
 var express = require('express');
-var http = require('http');
 var app = express();
-var server = http.createServer(app);
 
-app.get('/',function(req,res){
-  res.send('Hello World!');
+// Configuration
+
+app.configure(function() {
+  // app.engine() may not required for ejs, see docs for express 
+  app.engine('.html', require('ejs').renderFile);
+  // set path for later use app.get('views')
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  // without looking for the default layout.ejs
+  app.set('view options', { layout: false});
+
+});
+
+// Routes
+
+app.get('/', function(req,res) {
+  res.render('indexTemp.html');
 });
 
 app.listen(3000);
-console.log('running~', server.address());
+console.log('running~', app.get('address'));
 
